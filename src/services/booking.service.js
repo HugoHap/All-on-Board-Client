6266eeeb5c5199e9dcf0d9d2d9cbbd41dc30a29f
María Fdez de la Rosa <7mariadlrosa@gmail.com>
2022-05-10 15:58:33 +0200

    code review, servicios match, bookings

---
+++ src/services/booking.service.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+class BookingsService {
+
+    constructor() {
+        this.app = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}/bookings` })
+
+        this.api.interceptors.request.use((config) => {
+
+            const storedToken = localStorage.getItem("authToken");
+
+            if (storedToken) {
+                config.headers = { Authorization: `Bearer ${storedToken}` }
+            }
+
+            return config
+        })
+    }
+
+    getAllBookings = () => {
+        return this.app.get('/')
+    }
+
+    createBooking = booking => {
+        return this.app.post(`/${id}/create`, booking)
+    }
+
+    getOneBooking = id => {
+        return this.app.get(`/booking_${id}`)
+    }
+
+    editBooking = (id, bookingInfo) => {
+        return this.app.put(`/booking_${id}/edit`, bookingInfo)
+    }
+
+    deleteBooking = id => {
+        return this.app.delete(`/booking_${id}/delete`)
+    }
+
+}
+
+const bookingsService = new BookingsService()
+
+export default bookingsService
