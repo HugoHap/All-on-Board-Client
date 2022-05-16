90449f7589b16878e342a4e8908f1a8ee3c2dacd
Hugo Alvarez <hualvarez8@gmail.com>
2022-05-16 15:26:30 +0200

    Homepage carousel

--- src/components/BoardgamesList/BoardgamesList.jsx
+++ src/components/BoardgamesList/BoardgamesList.jsx
@@ -2,12 +2,12 @@ import { Row, Col } from "react-bootstrap"
 import BoardgameCard from "../BoardgameCard/BoardgameCard"
 
 const BoardgamesList = ({ boardgames }) => {
-
+console.log(boardgames);
     return (
         <>
             <Row>
                 {
-                    boardgames.map(game => {
+                    boardgames?.map(game => {
                         return (
                             <Col md={{ span: 4 }} key={game._id}>
                                 <BoardgameCard {...game} />
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,96 @@
-import { Container, Button } from 'react-bootstrap'
+import { Container, Button, Card, Carousel, Col, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import boardgameService from '../../services/boardgame.service';
+import BoardgameCard from '../../components/BoardgameCard/BoardgameCard';
 
 const IndexPage = () => {
+
+
+    const [rankGames, setRankGames] = useState([])
+
+    useEffect(() => {
+        boardgameService
+            .getOriginalBoardgames()
+            .then(({ data }) => {
+                setRankGames(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+            slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        }
+    };
+    console.log(rankGames[1]);
 
     return (
         <Container>
             <h1>WELCOME ON BOARD</h1>
             <hr />
-            <Link to="/boardgames">
+            {/* <Link to="/boardgames">
                 <Button variant="dark" size='lg'>BOARD GAMES</Button>
-            </Link>
+            </Link> */}
+
+            
+            <h4>RANKING TOP 8</h4>
+            <Carousel>
+                <Carousel.Item>
+                    <Row>
+                        {
+                            rankGames[1]?.slice(0, 4).map(elm => {
+
+                                return (<Col md={3}>
+                                    <Card className="BoardgameCard">
+                                        <Card.Img variant="top" src={elm.gameImg} />
+                                        <Card.Body>
+                                            <Card.Title>{elm.name}</Card.Title>
+                                            <div className="d-grid gap-2">
+                                                <Link to={`/boardgames/${elm._id}`} className="btn btn-dark">Details</Link>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                )
+                            })
+                        }
+                    </Row>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Row>
+                        {
+                            rankGames[1]?.slice(4, 8).map(elm => {
+
+                                return (<Col md={3}>
+                                    <Card className="BoardgameCard">
+                                        <Card.Img variant="top" src={elm.gameImg} />
+                                        <Card.Body>
+                                            <Card.Title>{elm.name}</Card.Title>
+                                            <div className="d-grid gap-2">
+                                                <Link to={`/boardgames/${elm._id}`} className="btn btn-dark">Details</Link>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                )
+                            })
+                        }
+                    </Row>
+                </Carousel.Item>
+            </Carousel>
         </Container>
     )
 }
