a4ef55ac2fdadd099efd51968eee1629c0e5a6bc
María Fdez de la Rosa <7mariadlrosa@gmail.com>
2022-05-19 18:58:12 +0200

    estilos

--- src/components/RentedProfileCard/RentedProfileCard.jsx
+++ src/components/RentedProfileCard/RentedProfileCard.jsx
@@ -1,25 +1,25 @@
 import { Card, Row, Container, Col } from 'react-bootstrap'
+import "./RentedProfileCard.css"
 
 const RentedProfileCard = ({ rentedGames }) => {
 
     return (
         <Container>
-            <h3>RENTED GAMES</h3>
             <Row>
                 <>
                     {
                         rentedGames?.map((elm) => {
-                            return <div key={elm._id}>
-                                <Col md={{ span: 6 }}>
-                                    <Card>
-                                        <Card.Body>
-                                            <Card.Title>{elm.boardGame.name}</Card.Title>
+                            return <Col md={{ span: 6 }} key={elm._id}>
+                                <Card className="CardBGRented">
+                                    <Card.Body>
+                                        <Card.Title className="CardBGRentTitle">{elm.boardGame.name}</Card.Title>
+                                        <div className="CardBGRentDescription">
                                             <p>Booked Date: {elm.startDate.slice(0, 10)} to {elm.endDate.slice(0, 10)} </p>
                                             <p>Contact Phone: {elm.boardGame.owner?.phone}</p>
-                                        </Card.Body>
-                                    </Card>
-                                </Col>
-                            </div>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
                         })
                     }
                 </>
--- src/pages/MatchDetailsPage/MatchDetailsPage.jsx
+++ src/pages/MatchDetailsPage/MatchDetailsPage.jsx
@@ -13,27 +13,27 @@ import "./MatchDetailsPage.css"
 
 
 const MatchDetailsPage = () => {
-    
+
     const [matchDetails, setMatchDetails] = useState()
     const [isLoading, setIsLoading] = useState(false)
-    
+
     const navigate = useNavigate()
-    
+
     const { id } = useParams()
-    
+
     useEffect(() => {
-        
+
         matchesService
-        .getOneMatch(id)
-        .then(({ data }) => {
-            console.log("mapaaaaaaaaa",data)
-            setMatchDetails(data)
-            setIsLoading(true)
-        })
-        .catch(err => console.log(err))
-        
+            .getOneMatch(id)
+            .then(({ data }) => {
+                console.log("mapaaaaaaaaa", data)
+                setMatchDetails(data)
+                setIsLoading(true)
+            })
+            .catch(err => console.log(err))
+
     }, [])
-    
+
     const MyMapComponent = withScriptjs(
         withGoogleMap((props) => (
             <GoogleMap defaultZoom={12} defaultCenter={{ lat: 40.415600407004575, lng: -3.6813260603979545 }}>
@@ -62,90 +62,97 @@ const MatchDetailsPage = () => {
 
 
         return (
-            <Container className="matchDetails">
-                <h1 className="matchDetailsTitle">
-                    "{boardGame.name}" match
-                </h1>
-                <hr className="hrMatch"></hr>
-                <div className="MatchCol">
-                    <Row>
-                        <Col md={{ span: 6 }}>
-                            <div className="matchDetailsDescription">
-                                <p>ORGANIZER: {organizer?.username}</p>
-                                <p>DESCRIPTION: {description}</p>
-                                <p>STARTING TIME: {startTime.slice(0, 10)}</p>
-                                {/* <p>Boardgame: {boardGame.name}</p> */}
-                                <p>AGE: {boardGame.age}</p>
-                                <p>PLAYERS: {boardGame.players.min}-{boardGame.players.max}</p>
-                            </div>
-                        </Col>
+            <div className="matchDiv">
+                <Container >
+                    <div className="matchDetails">
+                        <h1 className="matchDetailsTitle">
+                            "{boardGame.name}" match
+                        </h1>
+                        <hr className="hrMatch"></hr>
+                        <div className="MatchCol">
+                            <Row>
+                                <Col md={{ span: 6 }}>
+                                    <div className="matchDetailsDescription">
+                                        <p>ORGANIZER: {organizer?.username}</p>
+                                        <p>DESCRIPTION: {description}</p>
+                                        <p>STARTING TIME: {startTime.slice(0, 10)}</p>
+                                        {/* <p>Boardgame: {boardGame.name}</p> */}
+                                        <p>AGE: {boardGame.age}</p>
+                                        <p>PLAYERS: {boardGame.players.min}-{boardGame.players.max}</p>
+                                    </div>
+                                </Col>
 
-                        <Col md={{ span: 6 }}>
-                            <img style={{ width: '100%' }} src={boardGame.gameImg} alt={boardGame.name} />
-                        </Col>
-                        <Nav className="MatchesButtons">
-                            {
-                                !isLoggedIn ?
-                                    <>
-                                        <Link to="/match">
-                                            <Button variant="dark" className="btnReturn" >Back to Matches</Button>
-                                        </Link>
-
-                                    </>
-                                    :
-                                    <>
-                                        <Row>
-                                            <Col md={{ span: 6 }} className="ButtonsJoin">
-                                                <Button onClick={() => joinMatch(id)} variant="dark">Join Match</Button>
-                                            </Col>
-
-                                            <Col md={{ span: 5, offset: 1 }} className="ButtonsBack">
-                                                <div className="btnBack">
-                                                    <Link to="/match">
-                                                        <Button variant="dark">Back to Matches</Button>
-                                                    </Link>
-                                                </div>
-
-                                                <div className="ButtonsDelete">
-                                                    <Link to={`/match/${id}/delete`}>
-                                                        <Button>Delete Match</Button>
-                                                    </Link>
-                                                </div>
-                                            </Col>
-                                        </Row>
-                                    </>
-                            }
-                        </Nav>
-                        <div>
-                            <Container className='containerMatch'>
-                                <Row>
-                                    <>
+                                <Col md={{ span: 6 }} className="MatchesImg">
+                                    <img style={{ width: '100%' }} src={boardGame.gameImg} alt={boardGame.name} />
+                                </Col>
+                                <div className="MatchesButtons">
+                                    <Nav >
                                         {
-                                            players?.map((elm) => {
-                                                return <Col md={{ span: 3 }} className='playersMatch' key={elm._id}>
-                                                    <Card.Title>{elm.username} :) </Card.Title>
-                                                </Col>
-                                            })
+                                            !isLoggedIn ?
+                                                <>
+                                                    <Link to="/match">
+                                                        <Button variant="dark" className="btnReturn" >Back to Matches</Button>
+                                                    </Link>
+
+                                                </>
+                                                :
+                                                <>
+                                                    <Row>
+                                                        <Col md={{ span: 6 }} className="ButtonsJoin">
+                                                            <Button onClick={() => joinMatch(id)} variant="dark">Join Match</Button>
+                                                        </Col>
+
+                                                        <Col md={{ span: 5, offset: 1 }} className="ButtonsBack">
+                                                            <div className="btnBack">
+                                                                <Link to="/match">
+                                                                    <Button variant="dark">Back to Matches</Button>
+                                                                </Link>
+                                                            </div>
+
+                                                            <div className="ButtonsDelete">
+                                                                <Link to={`/match/${id}/delete`}>
+                                                                    <Button>Delete Match</Button>
+                                                                </Link>
+                                                            </div>
+                                                        </Col>
+                                                    </Row>
+                                                </>
                                         }
-                                    </>
-                                </Row>
-                            </Container>
-                        </div>
-                    </Row >
+                                    </Nav>
+                                </div>
+                                <div>
+                                    <Container className='containerMatch'>
+                                        <Row>
+                                            <>
+                                                {
+                                                    players?.map((elm) => {
+                                                        return <Col md={{ span: 3 }} className='playersMatch' key={elm._id}>
+                                                            <Card.Title>{elm.username} :) </Card.Title>
+                                                        </Col>
+                                                    })
+                                                }
+                                            </>
+                                        </Row>
+                                    </Container>
+                                </div>
+                            </Row >
 
 
-                </div >
+                        </div >
+                    </div>
 
-                <div className="mapMatch">
-                    <MyMapComponent
-                        isMarkerShown
-                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTfrEJjFOyJQ3p3WbSYP0yNoasqELJNFY&v=3.exp&libraries=geometry,drawing,places"
-                        loadingElement={<div style={{ height: `100%` }} />}
-                        containerElement={<div style={{ height: `400px` }} />}
-                        mapElement={<div style={{ height: `100%` }} />}
-                    />
-                </div>
-            </Container >
+                    <div className="mapMatch">
+
+                        <MyMapComponent
+                            isMarkerShown
+                            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTfrEJjFOyJQ3p3WbSYP0yNoasqELJNFY&v=3.exp&libraries=geometry,drawing,places"
+                            loadingElement={<div style={{ height: `100%` }} />}
+                            containerElement={<div style={{ height: `400px` }} />}
+                            mapElement={<div style={{ height: `100%` }} />}
+                        />
+                    </div>
+                </Container >
+            </div>
         )
     }
 }
